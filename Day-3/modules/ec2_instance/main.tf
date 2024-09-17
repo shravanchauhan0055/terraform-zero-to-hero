faae776f2cb9c77a3abfbbe7f3a1b38aaea86f69
Shravan Chauhan <135550611+shravanchauhan0055@users.noreply.github.com>
2024-09-18 04:04:46 +0530

    Update main.tf

--- Day-3/modules/ec2_instance/main.tf
+++ Day-3/modules/ec2_instance/main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
     ami = var.ami_value
+    key_name = "6sept"
     instance_type = var.instance_type_value
     tags = {
       Name = "modulevm1"
